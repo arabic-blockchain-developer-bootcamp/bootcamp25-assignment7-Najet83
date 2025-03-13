db7275e4d50f3754bd14af10ebfae220c63febb9
najet.benhamouda@cimf.tn <Yoga@coach39#>
2025-03-13 13:25:39 +0100

    Assignment 7

---
+++ bootcamp25-assignment7-Najet83/src/Assignment7.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Assignment7 is ERC20 {
+    // Constructor to initialize the token
+    // Fill in the parameters for the token name and symbol
+    constructor() ERC20("hope","hp") {}
+
+    // Function to mint tokens
+    // Fill in the visibility modifier to be external
+    // Set params as to address and amount
+    function mint(address to, uint256 amount) external{
+        // Fill in the logic
+        // call _mint function with to and amount params
+        _mint(to,amount);
+    }
+}
